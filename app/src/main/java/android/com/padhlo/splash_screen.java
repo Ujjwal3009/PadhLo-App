0922b584f9abf26e8dcbb5f4c6bb4572603d7c61
diablo1408 <shubhamb1408@gmail.com>
2020-10-05 19:49:03 +0530

    Book logo added with some animation for splash screen

--- app/src/main/java/android/com/padhlo/splash_screen.java
+++ app/src/main/java/android/com/padhlo/splash_screen.java
@@ -9,39 +9,29 @@ import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class splash_screen extends AppCompatActivity {
-    TextView img;
+    TextView text;
+    ImageView img;
     private final int SPLASH_DISPLAY_LENGTH = 3000;
+    Animation topAnim,bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        img = (TextView)findViewById(R.id.splash_image);
-        fadeOutAndHideImage(img);
-
-    }
-    // Create a method called fadeOutAndHideImage that takes in an ImageView
-    private void fadeOutAndHideImage(final TextView img)
-    {
-        // Fade Animation code
-        Animation fadeOut = new AlphaAnimation(1, 0);
-        fadeOut.setInterpolator(new AccelerateInterpolator());
-        fadeOut.setDuration(5000); // How long it takes for the animation to complete in milliseconds
-
-        fadeOut.setAnimationListener(new Animation.AnimationListener()
-        {
-            // Once the animation is done, set the visibility of the logo to GONE and navigate to the MainActivity after the set amount of time.
-            public void onAnimationEnd(Animation animation)
-            {
-                img.setVisibility(View.GONE);
-
-                /* New Handler to start the Menu-Activity
-                 * and close this Splash-Screen after some seconds.*/
-                new Handler().postDelayed(new Runnable(){
+        text = findViewById(R.id.splash_text);
+        img=findViewById(R.id.splash_image);
+        topAnim=AnimationUtils.loadAnimation(img.getContext(), R.anim.top_animation);
+        bottomAnim=AnimationUtils.loadAnimation(img.getContext(), R.anim.bottom_animation);
+        topAnim.setDuration(3000);
+        bottomAnim.setDuration(3000);
+        text.setAnimation(topAnim);
+        img.setAnimation(bottomAnim);
+        new Handler().postDelayed(new Runnable(){
                     @Override
                     public void run() {
                         /* Create an Intent that will start the Menu-Activity. */
@@ -49,12 +39,39 @@ public class splash_screen extends AppCompatActivity {
                         finish();
 
                     }
-                }, SPLASH_DISPLAY_LENGTH);
-            }
-            public void onAnimationRepeat(Animation animation) {}
-            public void onAnimationStart(Animation animation) {}
-        });
-
-        img.startAnimation(fadeOut);
+                    },SPLASH_DISPLAY_LENGTH);
     }
+    // Create a method called fadeOutAndHideImage that takes in an ImageView
+//    private void fadeOutAndHideImage(final TextView img)
+//    {
+//        // Fade Animation code
+//        Animation fadeOut = new AlphaAnimation(1, 0);
+//        fadeOut.setInterpolator(new AccelerateInterpolator());
+//        fadeOut.setDuration(5000); // How long it takes for the animation to complete in milliseconds
+//
+//        fadeOut.setAnimationListener(new Animation.AnimationListener()
+//        {
+//            // Once the animation is done, set the visibility of the logo to GONE and navigate to the MainActivity after the set amount of time.
+//            public void onAnimationEnd(Animation animation)
+//            {
+//                img.setVisibility(View.GONE);
+//
+//                /* New Handler to start the Menu-Activity
+//                 * and close this Splash-Screen after some seconds.*/
+//                new Handler().postDelayed(new Runnable(){
+//                    @Override
+//                    public void run() {
+//                        /* Create an Intent that will start the Menu-Activity. */
+//                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//                        finish();
+//
+//                    }
+//                }, SPLASH_DISPLAY_LENGTH);
+//            }
+//            public void onAnimationRepeat(Animation animation) {}
+//            public void onAnimationStart(Animation animation) {}
+//        });
+//
+//        img.startAnimation(fadeOut);
+//    }
 }
